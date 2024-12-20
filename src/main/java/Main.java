b4d90c8fd7fac553f8a3f0a9460b9f6c727e1d54
URGG <georgeurgiles2@gmail.com>
2024-12-20 12:30:55 -0800

    master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.print("$ ");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    do {
+    System.out.println(input + ": command not found");
+      System.out.println(input + ": command not found");
+      System.out.print("$ ");
+      input = scanner.nextLine();
+    } while (!input.matches(""));
+    scanner.close();
+  }
 }
