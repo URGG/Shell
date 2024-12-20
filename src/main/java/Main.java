e1fa000d4713347598a9841c2747053f6911b84d
URGG <georgeurgiles2@gmail.com>
2024-12-20 14:36:54 -0800

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ShellProgram {
+public class Main{
 
     private static String currentDirectory = System.getProperty("user.dir");
 
