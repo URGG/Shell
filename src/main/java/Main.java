6263bf19cfac79ef7c9afcc1c2fc90d58e7e41dc
URGG <georgeurgiles2@gmail.com>
2024-12-21 10:55:58 -0800

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main { // Renamed class to Main
+public class Main { // Class name is Main
     public static void main(String[] args) {
         // Simulating input for testing
         String input = "echo \"quz  hello\"  \"bar\""; // Example input
