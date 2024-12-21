afffe59bb6fe41d11fcdf9d6dc4d16338bedf120
URGG <georgeurgiles2@gmail.com>
2024-12-21 15:51:53 -0800

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,88 +1,153 @@
-import java.io.File;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HexFormat;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
+
 public class Main {
-  private static File currentDirectory =
-      new File(System.getProperty("user.dir"));
-  public static void main(String[] args) throws Exception {
-    while (true) {
-      System.out.print("$ ");
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      if (input.equals("exit 0")) {
-        scanner.close();
-        break;
-      } else if (input.startsWith("echo ")) {
-        System.out.println(input.substring(5));
-      } else if (input.startsWith("type ")) {
-        List<String> builtInCommands =
-            Arrays.asList("echo", "type", "exit", "pwd", "cd");
-        if (builtInCommands.contains(input.substring(5))) {
-          System.out.println(input.substring(5) + " is a shell builtin");
-        } else if (System.getenv("PATH") != null) {
-          String pathEnv = System.getenv("PATH");
-          String[] paths = pathEnv.split(":");
-          boolean found = false;
-          for (String path : paths) {
-            File file = new File(path + "/" + input.substring(5));
-            if (file.exists() && file.canExecute()) {
-              System.out.println(input.substring(5) + " is " +
-                                 file.getAbsolutePath());
-              found = true;
-              break;
+    private static void die(Exception e) {
+        e.printStackTrace(System.err);
+        System.exit(1);
+    }
+
+    private static void die(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }
+
+    private static void init() {
+        try {
+            FsObjectDatabase.init(Path.of("."));
+        } catch (Exception e) {
+            die(e);
+        }
+    }
+
+    private static String hex(byte[] bytes) {
+        var hash = new StringBuilder();
+        for (byte b : bytes)
+            hash.append("%02x".formatted(b));
+        return hash.toString();
+    }
+
+    private static byte[] fromHex(String hash) {
+        return HexFormat.of().parseHex(hash);
+    }
+
+    private static void catFile(List<String> opts) {
+        if (opts.size() != 2 || !opts.getFirst().equals("-p")) {
+            die("usage: git cat-file -p <sha>");
+        }
+        var sha = opts.get(1);
+        if (sha.length() != 40) {
+            die("bad sha: expected 40-byte sha-1");
+        }
+        try {
+            var git = FsObjectDatabase.init(Path.of("."));
+            try (var content = git.readBlob(fromHex(sha))) {
+                content.transferTo(System.out);
             }
-          }
-          if (!found) {
-            System.out.println(input.substring(5) + ": not found");
-          }
-        } else {
-          System.out.println(input.substring(5) + ": not found");
+        } catch (Exception e) {
+            die(e);
         }
-      } else if (input.equals("pwd")) {
-        System.out.println(currentDirectory.getAbsolutePath());
-      } else if (input.startsWith("cd")) {
-        String[] words = input.split(" ");
-        String path = words[1];
-        File newDirectory;
-        if (path.startsWith("/")) {
-          // Absolute path
-          newDirectory = new File(path);
-        } else if (path.equals("~")) {
-          
-          newDirectory = new File(System.getenv("HOME"));
-        } else {
-          // Relative path
-          newDirectory =
-              currentDirectory.toPath().resolve(path).normalize().toFile();
-        }
-        if (newDirectory.exists() && newDirectory.isDirectory()) {
-          currentDirectory = newDirectory;
-        } else {
-          System.out.println("cd: " + path + ": No such file or directory");
-        }
-      } else {
-        String command = input.split(" ")[0];
-        String path = getPath(command);
-        if (path == null) {
-          System.out.printf("%s: command not found%n", command);
-        } else {
-          String fullPath = path + input.substring(command.length());
-          Process p = Runtime.getRuntime().exec(fullPath.split(" "));
-          p.getInputStream().transferTo(System.out);
-        }
-      }
     }
-  }
-  private static String getPath(String input) {
-    for (String path : System.getenv("PATH").split(":")) {
-      Path file = Path.of(path, input);
-      if (Files.isReadable(file)) {
-        return file.toString();
-      }
+
+    private static void hashObject(List<String> opts) {
+        boolean write = false;
+        var path = Optional.<Path>empty();
+        for (var opt : opts) {
+            switch (opt) {
+                case "-w" -> write = true;
+                default -> path = Optional.of(Path.of(opt));
+            }
+        }
+        if (path.isEmpty()) {
+            die("usage: git hash-object [-w] <path>");
+        }
+        try (var s = Files.newInputStream(path.get())) {
+            var git = FsObjectDatabase.init(Path.of("."));
+            String hash = write
+                    ? hex(git.writeBlob(s, Files.size(path.get())))
+                    : hex(git.hashBlob(s, Files.size(path.get())));
+            System.out.println(hash);
+        } catch (Exception e) {
+            die(e);
+        }
     }
-    return null;
-  }
+
+    private static void lsTree(List<String> opts) {
+        if (opts.size() != 2) {
+            die("usage: git ls-tree --name-only <hash>");
+        }
+        try {
+            var git = FsObjectDatabase.init(Path.of("."));
+            for (var obj : git.listTree(fromHex(opts.get(1)))) {
+                System.out.println(obj.name());
+            }
+        } catch (Exception e) {
+            die(e);
+        }
+    }
+
+    private static void writeTree(List<String> opts) {
+        if (!opts.isEmpty()) {
+            die("usage: git write-tree");
+        }
+        try {
+            var git = FsObjectDatabase.init(Path.of("."));
+            System.out.println(hex(git.writeTree()));
+        } catch (Exception e) {
+            die(e);
+        }
+    }
+
+    private static void commitTree(List<String> opts) {
+        if (opts.size() != 5) {
+            die("usage: git commit-tree <tree_sha> -p <parent_commit_sha> -m <message>");
+        }
+        try {
+            var git = FsObjectDatabase.init(Path.of("."));
+            System.out.println(hex(git.commitTree(fromHex(opts.get(0)), List.of(fromHex(opts.get(2))), opts.get(4))));
+        } catch (Exception e) {
+            die(e);
+        }
+    }
+
+    private static void clone(List<String> opts) {
+        if (opts.size() != 2) {
+            die("usage: git clone <remote_url> <target_dir>");
+        }
+        String repositoryURL = opts.get(0);
+        String targetDir = opts.get(1);
+        try {
+            var url = new URI(repositoryURL);
+            var client = new GitSmartClient(url.getHost(), url.getPath());
+            var refs = client.listRefs();
+            for (byte[] ref : refs) {
+                System.out.println(hex(ref));
+            }
+        } catch (Exception e) {
+            die(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            die("usage: git <command>");
+        }
+        final String command = args[0];
+        final List<String> opts = Arrays.asList(args).subList(1, args.length);
+        switch (command) {
+            case "init" -> init();
+            case "cat-file" -> catFile(opts);
+            case "hash-object" -> hashObject(opts);
+            case "ls-tree" -> lsTree(opts);
+            case "write-tree" -> writeTree(opts);
+            case "commit-tree" -> commitTree(opts);
+            case "clone" -> clone(opts);
+            default -> System.out.println("Unknown command: " + command);
+        }
+    }
 }
